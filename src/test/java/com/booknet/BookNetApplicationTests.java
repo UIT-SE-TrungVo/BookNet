be08a30b6d50f4c168db131dd0627705f4e5704b
SeuitTrV <trungvo482@gmail.com>
2022-04-05 22:34:33 +0700

    Fix name

--- src/test/java/com/booknet/BookNetApplicationTests.java
+++ src/test/java/com/booknet/BookNetApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BooknetApplicationTests {
+class BookNetApplicationTests {
 
     @Test
     void contextLoads() {
